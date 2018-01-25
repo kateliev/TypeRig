39e22a886ca4b90f19e7e09c33e03750e8e91021
Vassil Kateliev <vassil@kateliev.com>
2018-01-25 15:22:15 +0200

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# MODULE: TypeRig
+# VER 	: 0.10
+# ----------------------------------------
+# (C) Vassil Kateliev, 2018 (http://www.kateliev.com)
+# (C) Karandash Type Foundry (http://www.karandash.eu)
+#-----------------------------------------
+# www.typerig.com
+
+# No warranties. By using this you agree
+# that you use it at your own risk!
+
+version = '0.10'
