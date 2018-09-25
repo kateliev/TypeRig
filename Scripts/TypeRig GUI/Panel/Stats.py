6a4bb2565b5b8d58536587dc8e1900b68b3bd1c6
Vassil Kateliev <vassil@kateliev.com>
2018-09-25 10:56:07 +0300

    Stats

--- Scripts/TypeRig GUI/Panel/Stats.py
+++ Scripts/TypeRig GUI/Panel/Stats.py
@@ -14,10 +14,11 @@ from PythonQt import QtCore, QtGui
 from typerig.proxy import pFont
 from typerig.glyph import eGlyph
 from typerig.gui import trTableView
-from collections import OrderedDict
+from typerig.brain import ratfrac
+#from collections import OrderedDict
 
 # - Init
-app_name, app_version = 'TypeRig | Glyph Statistics', '0.01'
+app_name, app_version = 'TypeRig | Glyph Statistics', '0.02'
 queries = ['Boundig Box Width (BBox)', 'Advance Width']
 
 # - Sub widgets ------------------------
@@ -36,15 +37,22 @@ class QGlyphInfo(QtGui.QVBoxLayout):
 		
 		self.edt_glyphName = QtGui.QLineEdit()
 		self.edt_glyphsSeq = QtGui.QLineEdit()
+		self.edt_glyphName.setToolTip('Current Glyph Name.') 
+		self.edt_glyphsSeq.setToolTip('Manual entry for Glyph names to populate stats info. Separated by SPACE') 
 		
 		self.cmb_query = QtGui.QComboBox()
 		self.cmb_charset = QtGui.QComboBox()
+		self.cmb_query.setToolTip('Select query type.')
+		self.cmb_charset.setToolTip('Select character set to compare with.')
 
 		self.cmb_query.addItems(queries)
 			
 		self.btn_refresh = QtGui.QPushButton('&Refresh')
 		self.btn_populate = QtGui.QPushButton('&Populate')
-		#self.btn_populate.setEnabled(False)
+		
+		# !!! Disable for now
+		self.cmb_charset.setEnabled(False)
+		self.btn_populate.setEnabled(False)
 
 		# -- Build Layout
 		self.lay_head.addWidget(QtGui.QLabel('G:'),	0,0,1,1)
@@ -59,12 +67,20 @@ class QGlyphInfo(QtGui.QVBoxLayout):
 		self.lay_head.addWidget(self.cmb_query,		3,1,1,7)
 		self.addLayout(self.lay_head)
 
-		# -- Node List Table
+		# -- Table
 		self.tab_stats = trTableView(self.table_dict)
+		self.refresh()
 		
+		# -- Note/Descriotion
 		self.addWidget(self.tab_stats)
-		#self.refresh() # Build Table
+		
+		note_msg = QtGui.QLabel('Note: Data is processed according to currently selected table row, serving as base for comparison (100%).')
+		note_msg.setOpenExternalLinks(True)
+		note_msg.setWordWrap(True)
 
+		self.addWidget(note_msg)
+
+		# -- Addons
 		self.btn_refresh.clicked.connect(lambda: self.refresh())
 		self.btn_populate.clicked.connect(lambda: self.populate())
 		self.cmb_query.currentIndexChanged.connect(lambda: self.change_query())
@@ -72,7 +88,8 @@ class QGlyphInfo(QtGui.QVBoxLayout):
 		# -- Table Styling
 		self.tab_stats.horizontalHeader().setStretchLastSection(False)
 		self.tab_stats.resizeColumnsToContents()
-		#self.tab_stats.selectionModel().selectionChanged.connect(self.change_selection())
+		self.tab_stats.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
+		self.tab_stats.selectionModel().selectionChanged.connect(self.change_selection)
 
 	def populate(self):
 		self.font = pFont()
@@ -86,23 +103,32 @@ class QGlyphInfo(QtGui.QVBoxLayout):
 		self.edt_glyphName.setText(eGlyph().name)
 		wLayers = self.glyph._prepareLayers((True, True, False, False))
 				
-		self.table_dict = {}
+		self.table_data = {}
+		self.table_proc = {}
 		node_count = 0
 
 		# - Populate table
-		self.table_dict[self.glyph.name] = {layer:self.glyph.getBounds(layer).width() for layer in wLayers}
+		self.table_data[self.glyph.name] = {layer:self.glyph.getBounds(layer).width() for layer in wLayers}
 		
 		if len(self.edt_glyphsSeq.text):
 			for glyph_name in self.edt_glyphsSeq.text.split(' '):
 				wGlyph = self.font.glyph(glyph_name)
-				self.table_dict[glyph_name] = {layer:wGlyph.getBounds(layer).width() for layer in wLayers}
+				self.table_data[glyph_name] = {layer:wGlyph.getBounds(layer).width() for layer in wLayers}
 			
-		self.tab_stats.setTable(self.table_dict)
+		self.tab_stats.setTable(self.table_data)
 		self.tab_stats.resizeColumnsToContents()		
 		
-	def changeLayer(self):
-		if self.doCheck():
-			self.refresh(self.cmb_query.currentText)
+	def change_selection(self):
+		base_index = self.tab_stats.selectionModel().selectedIndexes[0].row()
+		base_name = self.tab_stats.verticalHeaderItem(base_index).text()
+		
+		#print selection_name, self.table_data[selection_name]
+
+		for glyph_name, glyph_layers in self.table_data.iteritems():
+			self.table_proc[glyph_name] = {layer_name:'%s %%' %round(ratfrac(layer_value, self.table_data[base_name][layer_name]),2)  for layer_name, layer_value in glyph_layers.iteritems()}
+
+		self.tab_stats.setTable(self.table_proc)
+		self.tab_stats.resizeColumnsToContents()
 
 # - Tabs -------------------------------
 class tool_tab(QtGui.QWidget):
