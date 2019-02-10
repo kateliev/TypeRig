bd8e340b1421984077e646423264ba323dfc35d3
Vassil Kateliev <vassil@kateliev.com>
2019-02-11 00:13:29 +0200

    Corner Filter

--- Scripts/TypeRig GUI/Filter/Corner.py
+++ Scripts/TypeRig GUI/Filter/Corner.py
@@ -1,4 +1,4 @@
-#FLM: Glyph: Round corner
+#FLM: Filter: Corner
 # ----------------------------------------
 # (C) Vassil Kateliev, 2019 (http://www.kateliev.com)
 # (C) Karandash Type Foundry (http://www.karandash.eu)
@@ -17,17 +17,16 @@ import fontlab as fl6
 import fontgate as fgt
 from PythonQt import QtCore, QtGui
 from typerig.proxy import pFont, pShape, pNode, pWorkspace
+from typerig.node import eNode
 from typerig.glyph import eGlyph
 from typerig.gui import trTableView, trSliderCtrl
-
-
 
 # - Init
 global pLayers
 global pMode
 pLayers = None
 pMode = 0
-app_name, app_version = 'TypeRig | Round', '1.1'
+app_name, app_version = 'TypeRig | Corner', '1.5'
 
 # -- Strings
 filter_name = 'Smart corner'
@@ -45,6 +44,7 @@ class QSmartCorner(QtGui.QVBoxLayout):
 		self.font_masters = self.active_font.masters()
 		self.empty_preset = lambda row: OrderedDict([(row, OrderedDict([('Preset', 'Preset %s' %row)] + [(master, '0') for master in self.font_masters]))])
 		self.table_dict = self.empty_preset(0)
+		self.last_preset = 0
 
 		# -- Widgets
 		self.lay_head = QtGui.QGridLayout()
@@ -52,56 +52,200 @@ class QSmartCorner(QtGui.QVBoxLayout):
 		self.edt_glyphName = QtGui.QLineEdit()
 		self.edt_glyphName.setPlaceholderText('Glyph name')
 
+		# -- Buttons
 		self.btn_getBuilder = QtGui.QPushButton('Set &Builder')
 		self.btn_findBuilder = QtGui.QPushButton('&From Font')
 		self.btn_addPreset = QtGui.QPushButton('Add &Preset')
 		self.btn_delPreset = QtGui.QPushButton('&Remove Preset')
 		self.btn_loadPreset = QtGui.QPushButton('&Load Presets')
 		self.btn_savePreset = QtGui.QPushButton('&Save Presets')
-		self.btn_applyPreset = QtGui.QPushButton('&Apply Preset')
+		self.btn_apply_smartCorner = QtGui.QPushButton('&Apply Smart Corner')
+		self.btn_remove_smartCorner = QtGui.QPushButton('R&emove Smart Corner')
+
+		self.btn_apply_round = QtGui.QPushButton('&Round')
+		self.btn_apply_mitre = QtGui.QPushButton('&Mitre')
+		self.btn_apply_overlap = QtGui.QPushButton('&Overlap')
+		self.btn_apply_trap = QtGui.QPushButton('&Trap')
 
 		self.btn_getBuilder.setMinimumWidth(70)
 		self.btn_findBuilder.setMinimumWidth(70)
+		self.btn_apply_round.setMinimumWidth(70)
+		self.btn_apply_mitre.setMinimumWidth(70)
+		self.btn_apply_overlap.setMinimumWidth(70)
+		self.btn_apply_trap.setMinimumWidth(70)
+
 		self.btn_addPreset.setMinimumWidth(140)
 		self.btn_delPreset.setMinimumWidth(140)
 		self.btn_loadPreset.setMinimumWidth(140)
 		self.btn_savePreset.setMinimumWidth(140)
-		self.btn_applyPreset.setMinimumWidth(140)
+		self.btn_apply_smartCorner.setMinimumWidth(140)
+		self.btn_remove_smartCorner.setMinimumWidth(140)
 
 		self.btn_getBuilder.setCheckable(True)
 		self.btn_getBuilder.setChecked(False)
 		self.btn_findBuilder.setEnabled(False)
+		self.btn_apply_round.setEnabled(False)
 
 		self.btn_getBuilder.clicked.connect(self.getBuilder)
 		self.btn_addPreset.clicked.connect(lambda: self.preset_modify(False))
 		self.btn_delPreset.clicked.connect(lambda: self.preset_modify(True))
 		self.btn_loadPreset.clicked.connect(lambda: self.preset_load())
 		self.btn_savePreset.clicked.connect(lambda: self.preset_save())
-		self.btn_applyPreset.clicked.connect(lambda: self.preset_apply())
 
-		# -- Rounding recipe Table
-		self.tab_roundValues = trTableView(None)		
-		self.tab_roundValues.clear()
-		self.tab_roundValues.setTable(self.table_dict, sortData=(False, False))
-		self.tab_roundValues.horizontalHeader().setStretchLastSection(False)
-		self.tab_roundValues.verticalHeader().hide()
-		#self.tab_roundValues.resizeColumnsToContents()
+		self.btn_apply_smartCorner.clicked.connect(lambda: self.apply_SmartCorner(False))
+		self.btn_remove_smartCorner.clicked.connect(lambda: self.apply_SmartCorner(True))
+		
+		#self.btn_apply_round.clicked.connect(lambda: self.apply_round())
+		self.btn_apply_mitre.clicked.connect(lambda: self.apply_mitre(False))
+		self.btn_apply_overlap.clicked.connect(lambda: self.apply_mitre(True))
+		self.btn_apply_trap.clicked.connect(lambda: self.apply_trap())
+
+		# -- Preset Table
+		self.tab_presets = trTableView(None)		
+		self.tab_presets.clear()
+		self.tab_presets.setTable(self.table_dict, sortData=(False, False))
+		self.tab_presets.horizontalHeader().setStretchLastSection(False)
+		self.tab_presets.verticalHeader().hide()
+		#self.tab_presets.resizeColumnsToContents()
 
 		# -- Build Layout
-		self.lay_head.addWidget(QtGui.QLabel('Round: Smart corner'), 0,0,1,8)
-		self.lay_head.addWidget(QtGui.QLabel('B: '),			1,0,1,1)
-		self.lay_head.addWidget(self.edt_glyphName,				1,1,1,3)
-		self.lay_head.addWidget(self.btn_getBuilder,			1,4,1,2)
-		self.lay_head.addWidget(self.btn_findBuilder,			1,6,1,2)
-		self.lay_head.addWidget(self.btn_loadPreset,			2,0,1,4)
-		self.lay_head.addWidget(self.btn_savePreset,			2,4,1,4)
-		self.lay_head.addWidget(self.btn_addPreset,				3,0,1,4)
-		self.lay_head.addWidget(self.btn_delPreset,				3,4,1,4)
-		self.lay_head.addWidget(self.tab_roundValues,			4,0,2,8)
-		self.lay_head.addWidget(self.btn_applyPreset,			6,0,1,8)
+		self.lay_head.addWidget(QtGui.QLabel('Value Presets:'), 0,0,1,8)
+		self.lay_head.addWidget(self.btn_loadPreset,			1,0,1,4)
+		self.lay_head.addWidget(self.btn_savePreset,			1,4,1,4)
+		self.lay_head.addWidget(self.btn_addPreset,				2,0,1,4)
+		self.lay_head.addWidget(self.btn_delPreset,				2,4,1,4)
+		self.lay_head.addWidget(self.tab_presets,				3,0,5,8)
+
+		self.lay_head.addWidget(QtGui.QLabel('Corner Actions:'),10, 0, 1, 8)
+		self.lay_head.addWidget(self.btn_apply_round,			11, 0, 1, 2)
+		self.lay_head.addWidget(self.btn_apply_mitre,			11, 2, 1, 2)
+		self.lay_head.addWidget(self.btn_apply_overlap,			11, 4, 1, 2)
+		self.lay_head.addWidget(self.btn_apply_trap,			11, 6, 1, 2)
+
+		self.lay_head.addWidget(QtGui.QLabel('Smart Corner:'),13,0,1,8)
+		self.lay_head.addWidget(QtGui.QLabel('Builder: '),			14,0,1,1)
+		self.lay_head.addWidget(self.edt_glyphName,				14,1,1,3)
+		self.lay_head.addWidget(self.btn_getBuilder,			14,4,1,2)
+		self.lay_head.addWidget(self.btn_findBuilder,			14,6,1,2)
+		self.lay_head.addWidget(self.btn_apply_smartCorner,		15,0,1,4)
+		self.lay_head.addWidget(self.btn_remove_smartCorner,	15,4,1,4)
 
 		self.addLayout(self.lay_head)
 
+	# - Presets management ------------------------------------------------
+	def preset_modify(self, delete=False):
+		table_rawList = self.tab_presets.getTable(raw=True)
+		
+		if delete:
+			for selection in self.tab_presets.selectionModel().selectedIndexes:
+				table_rawList.pop(selection.row())
+				print selection.row()
+
+		new_entry = OrderedDict()
+		
+		for key, data in table_rawList:
+			new_entry[key] = OrderedDict(data)
+
+		if not delete: new_entry[len(table_rawList)] = self.empty_preset(len(table_rawList)).items()[0][1]
+		self.tab_presets.setTable(new_entry, sortData=(False, False))
+
+	def preset_load(self):
+		fontPath = os.path.split(self.active_font.fg.path)[0]
+		fname = QtGui.QFileDialog.getOpenFileName(self.upper_widget, 'Load presets from file', fontPath, 'TypeRig JSON (*.json)')
+		
+		if fname != None:
+			with open(fname, 'r') as importFile:
+				imported_data = json.load(importFile)
+			
+			# - Convert Data
+			new_data = OrderedDict()
+			for key, data in imported_data:
+				new_data[key] = OrderedDict(data)
+
+			self.tab_presets.setTable(new_data, sortData=(False, False))
+			print 'LOAD:\t Font:%s; Presets loaded from: %s.' %(self.active_font.name, fname)
+
+	def preset_save(self):
+		fontPath = os.path.split(self.active_font.fg.path)[0]
+		fname = QtGui.QFileDialog.getSaveFileName(self.upper_widget, 'Save presets to file', fontPath, 'TypeRig JSON (*.json)')
+		
+		if fname != None:
+			with open(fname, 'w') as exportFile:
+				json.dump(self.tab_presets.getTable(raw=True), exportFile)
+
+			print 'SAVE:\t Font:%s; Presets saved to: %s.' %(self.active_font.name, fname)
+
+	def getPreset(self):
+		table_raw = self.tab_presets.getTable(raw=True)
+		active_preset_index = self.tab_presets.selectionModel().selectedIndexes[0].row()
+		
+		if active_preset_index is None: 
+			active_preset_index = self.last_preset
+		else:
+			self.last_preset = active_preset_index
+
+		return dict(table_raw[active_preset_index][1][1:])
+
+	# - Glyphs ----------------------------------------------------
+	def getProcessGlyphs(self):
+		process_glyphs = []
+		active_workspace = pWorkspace()
+			
+		# - Collect process glyphs
+		if pMode == 0: process_glyphs.append(eGlyph()) # Current Active Glyph
+		if pMode == 1: process_glyphs = [eGlyph(glyph) for glyph in active_workspace.getTextBlockGlyphs()] # All glyphs in current window
+		
+		return process_glyphs
+
+	# - Basic Corner ------------------------------------------------
+	def apply_mitre(self, doKnot=False):
+		# - Init
+		process_glyphs = self.getProcessGlyphs()
+		active_preset = self.getPreset()	
+
+		# - Process
+		if len(process_glyphs):
+			for glyph in process_glyphs:
+				if glyph is not None:
+					wLayers = glyph._prepareLayers(pLayers)
+		
+					for layer in wLayers:
+						if layer in active_preset.keys():
+							selection = glyph.selectedNodes(layer, filterOn=True, extend=eNode, deep=True)
+							
+							for node in reversed(selection):
+								if not doKnot:
+									node.cornerMitre(float(active_preset[layer]))
+								else:
+									node.cornerMitre(-float(active_preset[layer]), True)
+					
+					action = 'Mitre Corner' if not doKnot else 'Overlap Corner'
+					glyph.update()
+					glyph.updateObject(glyph.fl, '%s @ %s.' %(action, '; '.join(active_preset.keys())))
+
+	def apply_trap(self):
+		# - Init
+		process_glyphs = self.getProcessGlyphs()
+		active_preset = self.getPreset()	
+
+		# - Process
+		if len(process_glyphs):
+			for glyph in process_glyphs:
+				if glyph is not None:
+					wLayers = glyph._prepareLayers(pLayers)
+		
+					for layer in wLayers:
+						if layer in active_preset.keys():
+							selection = glyph.selectedNodes(layer, filterOn=True, extend=eNode, deep=True)
+							
+							for node in reversed(selection):
+								preset_values = tuple([float(item.strip()) for item in active_preset[layer].split(',')])
+								node.cornerTrap(*preset_values)
+					
+					glyph.update()
+					glyph.updateObject(glyph.fl, '%s @ %s.' %('Ink Trap', '; '.join(active_preset.keys())))
+
+	# - Smart Corner ------------------------------------------------
 	def getBuilder(self):
 		if self.btn_getBuilder.isChecked():
 			if len(self.edt_glyphName.text):
@@ -119,51 +263,9 @@ class QSmartCorner(QtGui.QVBoxLayout):
 		else:
 			self.builder = None
 			self.edt_glyphName.clear()
-			self.btn_getBuilder.setText('Set Builder')			
+			self.btn_getBuilder.setText('Set Builder')	
 
-	def preset_modify(self, delete=False):
-		table_rawList = self.tab_roundValues.getTable(raw=True)
-		
-		if delete:
-			for selection in self.tab_roundValues.selectionModel().selectedIndexes:
-				table_rawList.pop(selection.row())
-				print selection.row()
-
-		new_entry = OrderedDict()
-		
-		for key, data in table_rawList:
-			new_entry[key] = OrderedDict(data)
-
-		if not delete: new_entry[len(table_rawList)] = self.empty_preset(len(table_rawList)).items()[0][1]
-		self.tab_roundValues.setTable(new_entry, sortData=(False, False))
-
-	def preset_load(self):
-		fontPath = os.path.split(self.active_font.fg.path)[0]
-		fname = QtGui.QFileDialog.getOpenFileName(self.upper_widget, 'Load presets from file', fontPath, 'TypeRig JSON (*.json)')
-		
-		if fname != None:
-			with open(fname, 'r') as importFile:
-				imported_data = json.load(importFile)
-			
-			# - Convert Data
-			new_data = OrderedDict()
-			for key, data in imported_data:
-				new_data[key] = OrderedDict(data)
-
-			self.tab_roundValues.setTable(new_data, sortData=(False, False))
-			print 'LOAD:\t Font:%s; Presets loaded from: %s.' %(self.active_font.name, fname)
-
-	def preset_save(self):
-		fontPath = os.path.split(self.active_font.fg.path)[0]
-		fname = QtGui.QFileDialog.getSaveFileName(self.upper_widget, 'Save presets to file', fontPath, 'TypeRig JSON (*.json)')
-		
-		if fname != None:
-			with open(fname, 'w') as exportFile:
-				json.dump(self.tab_roundValues.getTable(raw=True), exportFile)
-
-			print 'SAVE:\t Font:%s; Presets saved to: %s.' %(self.active_font.name, fname)
-
-	def __setFilter(self, glyph, shape, layer, builder, suffix='.old'):
+	def process_setFilter(self, glyph, shape, layer, builder, suffix='.old'):
 		new_container = fl6.flShape()
 		new_container.shapeData.name = shape.shapeData.name
 		shape.shapeData.name = shape.shapeData.name + suffix
@@ -175,7 +277,7 @@ class QSmartCorner(QtGui.QVBoxLayout):
 		new_container.update()
 		glyph.layer(layer).addShape(new_container)
 
-	def __processGlyph(self, glyph, preset):
+	def process_smartCorner(self, glyph, preset):
 		wLayers = glyph._prepareLayers(pLayers)					
 		
 		for layer in wLayers:
@@ -193,9 +295,9 @@ class QSmartCorner(QtGui.QVBoxLayout):
 					if len(glyph.containers(layer)):
 						for container in glyph.containers(layer):
 							if shape not in container.includesList: 
-								self.__setFilter(glyph, shape, layer, self.builder)
+								self.process_setFilter(glyph, shape, layer, self.builder)
 					else:
-						self.__setFilter(glyph, shape, layer, self.builder)
+						self.process_setFilter(glyph, shape, layer, self.builder)
 
 				# - Process the nodes
 				process_nodes = [pNode(node) for shape, node_list in nodes_at_shapes for node in node_list]
@@ -211,25 +313,16 @@ class QSmartCorner(QtGui.QVBoxLayout):
 		glyph.update()
 		glyph.updateObject(glyph.fl, 'DONE:\t Glyph: %s; Filter: Smart corner; Parameters: %s' %(glyph.name, preset))
 
-	def preset_apply(self):
+	def apply_SmartCorner(self, remove=False):
 		if self.builder is not None:
 			# - Init
-			process_glyphs = []
-			table_raw = self.tab_roundValues.getTable(raw=True)
-			active_preset_index = self.tab_roundValues.selectionModel().selectedIndexes[0].row()
-			if active_preset_index is None: active_preset_index = 0
-			active_preset = dict(table_raw[active_preset_index][1][1:])
-			active_workspace = pWorkspace()
-			
-			# - Collect process glyphs
-			if pMode == 0: process_glyphs.append(eGlyph()) # Current Active Glyph
-			if pMode == 1: process_glyphs = [eGlyph(glyph) for glyph in active_workspace.getTextBlockGlyphs()] # All glyphs in current window
-			if pMode > 1: return # Not allowed - exit!
+			process_glyphs = self.getProcessGlyphs()
+			active_preset = self.getPreset()			
 			
 			# - Process
 			if len(process_glyphs):
 				for glyph in process_glyphs:
-					if glyph is not None: self.__processGlyph(glyph, active_preset)
+					if glyph is not None: self.process_smartCorner(glyph, active_preset)
 
 		else:
 			print 'ERROR:\t Please specify a Glyph with suitable Shape Builder (Smart corner) first!'
