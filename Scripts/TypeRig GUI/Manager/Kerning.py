cd1d83e5ad39198a67e8467bcce8859f6e38282b
Vassil Kateliev <vassil@kateliev.com>
2020-07-27 14:12:33 +0300

    Update Kerning.py

--- Scripts/TypeRig GUI/Manager/Kerning.py
+++ Scripts/TypeRig GUI/Manager/Kerning.py
@@ -10,11 +10,11 @@
 # - Init
 global pLayers
 pLayers = None
-app_name, app_version = 'TypeRig | Kerning Overview', '1.1'
+app_name, app_version = 'TypeRig | Kerning Overview', '1.5'
 alt_mark = '.'
 
 # - Dependencies -----------------
-import os, json
+import os, json, re
 from platform import system
 
 import fontlab as fl6
@@ -29,30 +29,12 @@ from typerig.proxy import *
 fileFormats = ['TypeRig JSON Raw Kerning (*.json)', 'FontLab JSON Kernign (*.vfm)']
 NOVAL = 'NOVAL'
 pair_delimiter = '|'
+pair_class = '@'
 command_separator = ';'
 command_operator = '== >= <= != > <'.split()
 
 # - Functions ----------------------------------------------------------------
-def json_class_dumb_decoder(jsonData):
-	retund_dict = {}
-	pos_dict = {(True, False):'KernLeft', (False, True):'KernRight', (True, True):'KernBothSide'}
-	getPos = lambda d: (d['1st'] if d.has_key('1st') else False , d['2nd'] if d.has_key('2nd') else False)
-
-	if len(jsonData.keys()):
-		if jsonData.has_key('masters') and len(jsonData['masters']):
-			for master in jsonData['masters']:
-				if len(master.keys()):
-					if master.has_key('kerningClasses') and len(master['kerningClasses']):
-						temp_dict = {}
-
-						for group in master['kerningClasses']:
-							if group.has_key('names'):
-								temp_dict[group['name']] = (group['names'], pos_dict[getPos(group)])
-
-						retund_dict[master['name']] = temp_dict
-	return retund_dict
-
-def parser_highlight(parse_string):
+def parser_format(parse_string):
 	parse_string = [item.strip() for item in parse_string.split(command_separator)]
 	return_commands = []
 	
@@ -65,22 +47,36 @@ def parser_highlight(parse_string):
 				return_commands.append((item[opi:], color_rule))
 	return return_commands
 
+def parser_highlight(parse_string):
+	parse_string = [item.strip() for item in parse_string.split(command_separator)]
+	return_commands = []
+
+	for item in parse_string:
+		item_tuple = item.split(pair_delimiter)
+		return_commands.append((item_tuple[0].strip(), item_tuple[1].strip()))
+
+	return return_commands
+
 # - Custom classes -----------------------------------------------------------
 class KernTableWidget(QtGui.QTableWidget):
 	def __init__(self, aux):
 		super(KernTableWidget, self).__init__()
 
 		# - Init
-		self.flag_valueChanged = QtGui.QColor('powderblue')
-		self.header = self.horizontalHeader()
 		self.aux = aux
-
-		# - Behavior
+		
+		self.flag_valueDefault = QtGui.QColor('black')
+		self.flag_valueChanged = QtGui.QColor('red')
+		
+		self.values_changed = []
 		self.itemChanged.connect(self.kern_value_changed)
 
+
 		# - Styling
+		self.header = self.horizontalHeader()
 		self.setShowGrid(True)
-		self.setSortingEnabled(True)
+		#self.setSortingEnabled(True)
+		#self.setSelectionMode(QtGui.QAbstractItemView.MultiSelection)
 
 	def setTable(self, data):
 		# - Init
@@ -90,7 +86,7 @@ class KernTableWidget(QtGui.QTableWidget):
 
 		self.setColumnCount(len(font_layers))
 		self.setRowCount(len(all_pairs))
-		self.setSortingEnabled(False) # Great solution from: https://stackoverflow.com/questions/7960505/strange-qtablewidget-behavior-not-all-cells-populated-after-sorting-followed-b
+		#self.setSortingEnabled(False) # Great solution from: https://stackoverflow.com/questions/7960505/strange-qtablewidget-behavior-not-all-cells-populated-after-sorting-followed-b
 
 		# - Headers 
 		self.setHorizontalHeaderLabels(font_layers)
@@ -110,7 +106,7 @@ class KernTableWidget(QtGui.QTableWidget):
 				self.setItem(row, col, new_item)
 
 		self.blockSignals(False)
-		self.setSortingEnabled(True)
+		#self.setSortingEnabled(True)
 		self.resizeRowsToContents()
 
 	def getSelection(self):
@@ -121,14 +117,17 @@ class KernTableWidget(QtGui.QTableWidget):
 
 	def kern_value_changed(self, item):
 		current_row, current_col = item.row(), item.column()
-		current_layer = self.horizontalHeaderItem(current_col).text()
-		current_pair = tuple(self.verticalHeaderItem(current_col).text().split(pair_delimiter))
+		#current_layer = self.horizontalHeaderItem(current_col).text()
+		#current_pair = tuple(self.verticalHeaderItem(current_col).text().split(pair_delimiter))
+		current_layer = self.aux.tab_fontKerning_data[0][current_col]
+		current_pair = self.aux.tab_fontKerning_data[2][current_row]
+		
 		self.aux.active_font.kerning(current_layer)[current_pair] = int(item.text())
-		item.setForeground(QtGui.QBrush(QtGui.QColor('red')))
+		item.setForeground(QtGui.QBrush(self.flag_valueChanged))
+		
 		if self.aux.btn_fontKerning_autoupdate.isChecked():
 			self.aux.active_font.update()
 		
-
 
 # - Font Group Kerning -------------------------------------------------------
 class WKernGroups(QtGui.QWidget):
@@ -167,8 +166,20 @@ class WKernGroups(QtGui.QWidget):
 		# -- Cell highlighter and search ------
 		self.btn_search_pair_under = QtGui.QPushButton('Current Pair')
 		self.edt_search_pair = QtGui.QLineEdit()
-		self.btn_search_pair = QtGui.QPushButton('Find')
+		self.edt_search_pair.setPlaceholderText('Pair serach example: A|V; @O|@A; Where A|V plain pair and @O|@A classes containing O and A.')
+		self.edt_search_regex = QtGui.QLineEdit()
+		self.edt_search_regex.setPlaceholderText('RegEx search example: .*.O.*.A.*; Note: Pair source is [space] separated!')
+
+		self.btn_search_pair = QtGui.QPushButton('Find Pairs')
+		self.btn_search_regex = QtGui.QPushButton('Find RegEx')
+
+		self.btn_search_hide = QtGui.QPushButton('Hide others')
+		self.btn_search_hide.setCheckable(True)
+		
+		self.btn_search_hide.clicked.connect(lambda: self.update_table_show_all())
 		self.btn_search_pair_under.clicked.connect(lambda: self.update_table_hightlights(False))
+		self.btn_search_pair.clicked.connect(lambda: self.update_table_hightlights(True))
+		self.btn_search_regex.clicked.connect(lambda: self.update_table_regex())
 
 		# -- Table ----------------------------
 		self.btn_fontKerning_autoupdate = QtGui.QPushButton('Auto Update')
@@ -176,7 +187,7 @@ class WKernGroups(QtGui.QWidget):
 		self.tab_fontKerning = KernTableWidget(self)
 		self.btn_fontKerning_autoupdate.setCheckable(True)
 		self.btn_fontKerning_autoupdate.setChecked(True)
-		self.btn_fontKerning_update.clicked.connect(lambda: self.active_font.update())
+		self.btn_fontKerning_update.clicked.connect(lambda: self.update_font())
 
 		# - Menus & Actions --------------------
 		# -- Main Database actions
@@ -235,18 +246,27 @@ class WKernGroups(QtGui.QWidget):
 			self.lay_grid.addWidget(self.btn_mac_data_reset,				0, 20, 1, 5)			
 
 		# -- Regular interface
-		self.lay_grid.addWidget(self.btn_search_pair_under,					1, 0, 1, 10)
-		self.lay_grid.addWidget(self.edt_search_pair,						1, 10, 1, 10)	
+		
+		self.lay_grid.addWidget(self.edt_search_pair,						1, 0, 1, 20)	
 		self.lay_grid.addWidget(self.btn_search_pair,						1, 20, 1, 5)	
-		self.lay_grid.addWidget(self.btn_fontKerning_autoupdate,			1, 30, 1, 5)
-		self.lay_grid.addWidget(self.btn_fontKerning_update,				1, 35, 1, 5)
+		self.lay_grid.addWidget(self.btn_search_pair_under,					1, 25, 1, 5)
+		self.lay_grid.addWidget(self.btn_search_hide,						2, 25, 1, 5)
+
+		self.lay_grid.addWidget(self.edt_search_regex,						2, 0, 1, 20)	
+		self.lay_grid.addWidget(self.btn_search_regex,						2, 20, 1, 5)
+		
+		self.lay_grid.addWidget(self.btn_fontKerning_autoupdate,			1, 35, 1, 5)
+		self.lay_grid.addWidget(self.btn_fontKerning_update,				2, 35, 1, 5)
+		
 		self.lay_grid.addWidget(self.tab_fontKerning,						4, 0, 32, 40)
+
 		self.lay_grid.addWidget(self.cmb_select_color,						36, 0, 1, 5)
 		self.lay_grid.addWidget(self.btn_formatting_color,					36, 5, 1, 1)
 		self.lay_grid.addWidget(self.edt_formatting,						36, 6, 1, 24)
 		self.lay_grid.addWidget(self.btn_formatting_apply,					36, 30, 1, 5)
 		self.lay_grid.addWidget(self.btn_formatting_clear,					36, 35, 1, 5)
 		
+		self.lay_grid.setSpacing(3)
 		self.setLayout(self.lay_grid)
 	
 	# - Context Menu Procedures --------------------------------------
@@ -266,6 +286,14 @@ class WKernGroups(QtGui.QWidget):
 
 
 	# - Main Procedures --------------------------------------------
+	def update_font(self):
+		'''
+		for item in self.tab_fontKerning.values_changed:
+			item.setForeground(QtGui.QBrush(self.tab_fontKerning.flag_valueDefault))
+		'''
+		self.active_font.update()
+		#self.tab_fontKerning.values_changed = []
+
 	def update_data(self, source, updateTable=True):
 		self.tab_fontKerning_data = source
 		
@@ -275,11 +303,11 @@ class WKernGroups(QtGui.QWidget):
 			self.tab_fontKerning.setTable(self.tab_fontKerning_data)
 
 	def update_table_format(self, clearTable=False):
-		formattig_list = parser_highlight(self.edt_formatting.text)
+		formattig_list = parser_format(self.edt_formatting.text)
 		self.tab_fontKerning.blockSignals(True)
 
-		for row in range(self.tab_fontKerning.rowCount):
-			for col in range(self.tab_fontKerning.columnCount):
+		for row in xrange(self.tab_fontKerning.rowCount):
+			for col in xrange(self.tab_fontKerning.columnCount):
 				cell_item = self.tab_fontKerning.item(row, col)
 				
 				if cell_item is not None:	
@@ -292,21 +320,88 @@ class WKernGroups(QtGui.QWidget):
 
 		self.tab_fontKerning.blockSignals(False)
 			
-	def update_table_hightlights(self, search_field=False):
+	def update_table_hightlights(self, search_field=False, hide_others=False):
+		# !!! TODO: Use direct Vertical/Horizontal header indexing if columns/rows are moved
+		show_items = []
+
 		font_layers, font_kerning, all_pairs = self.tab_fontKerning_data
+		col = font_layers.index(pGlyph().layer().name)
+		proxy_kerning = pKerning(font_kerning[col])
+		
+		self.tab_fontKerning.setSelectionMode(QtGui.QAbstractItemView.MultiSelection)
 		self.tab_fontKerning.clearSelection()
 
-		if not search_field: # !!! TODO: Use direct Vertical/Horizontal header indexing if columns/rows are moved
-			pair_under_cursor = self.application.getActiveKernPair()
+		if not search_field: 
+			pairs_list = [self.application.getActiveKernPair()]
+		else:
+			pairs_list = parser_highlight(self.edt_search_pair.text)
 
-			if pair_under_cursor is not None:
-				col = font_layers.index(pGlyph().layer().name)
-				proxy_kerning = pKerning(font_kerning[col])
-				row = all_pairs.index(proxy_kerning.getPairGroups(pair_under_cursor))
-			 	
-			 	selected_item = self.tab_fontKerning.item(row, col)
-			 	self.tab_fontKerning.setRangeSelected(QtGui.QTableWidgetSelectionRange(row, col, row, col), True)
-			 	self.tab_fontKerning.scrollToItem(selected_item, QtGui.QAbstractItemView.PositionAtCenter)
+		for pair in pairs_list:
+			if pair is not None:
+				left, right = pair
+				left_is_group, right_is_group = False, False
+				
+				if pair_class in left:
+					left = left.strip(pair_class)
+					left_is_group = True
+
+				if pair_class in right:
+					right = right.strip(pair_class)
+					right_is_group = True
+
+				if not search_field:
+					left_is_group = True
+					right_is_group = True
+
+				new_left, new_right = proxy_kerning.getPairGroups((left, right))
+				new_left = new_left if left_is_group else left
+				new_right = new_right if right_is_group else right
+
+				row = all_pairs.index((new_left, new_right))
+				
+				selected_item = self.tab_fontKerning.item(row, col)
+				show_items.append(row)
+				
+				if not search_field:
+					self.tab_fontKerning.setRangeSelected(QtGui.QTableWidgetSelectionRange(row, col, row, col), True)
+					self.tab_fontKerning.scrollToItem(selected_item, QtGui.QAbstractItemView.PositionAtCenter)
+				else:
+					self.tab_fontKerning.selectRow(row)
+
+		if self.btn_search_hide.isChecked():
+			for row in xrange(self.tab_fontKerning.rowCount):
+				if row in show_items:
+					self.tab_fontKerning.showRow(row)
+				else:	
+					self.tab_fontKerning.hideRow(row)
+
+		self.tab_fontKerning.setSelectionMode(QtGui.QAbstractItemView.ExtendedSelection) # Back to regular selection
+
+	def update_table_regex(self):
+		all_pairs = self.tab_fontKerning_data[2]
+		all_pairs_text = [' '.join(item) for item in all_pairs]
+		results = re.findall(self.edt_search_regex.text, '\n'.join(all_pairs_text), re.UNICODE)
+		show_items = []
+
+		for search_result in results:
+			if search_result in all_pairs_text:
+				row = all_pairs_text.index(search_result)
+				self.tab_fontKerning.selectRow(row)
+				show_items.append(row)
+			else:
+				print 'RegEx:\tNot Found: %s' %search_result
+
+		if self.btn_search_hide.isChecked():
+			for row in xrange(self.tab_fontKerning.rowCount):
+				if row in show_items:
+					self.tab_fontKerning.showRow(row)
+				else:	
+					self.tab_fontKerning.hideRow(row)
+		
+
+	def update_table_show_all(self):
+		for row in xrange(self.tab_fontKerning.rowCount):
+			self.tab_fontKerning.showRow(row)
 
 	def kerning_reset(self):
 		self.active_font.reset_kerning_groups(self.cmb_layer.currentText)
@@ -318,11 +413,13 @@ class WKernGroups(QtGui.QWidget):
 		font_layers = []
 		pair_set = []
 		print 'LOAD:\tPreparing kerning data...'
+		import_empty = QtGui.QMessageBox.question(self, 'Import kerning from font', 'Do you want to import layers with empty/no kerning', QtGui.QMessageBox.Yes, QtGui.QMessageBox.No)
 		
 		for layer in self.active_font.masters():
 			current_kerning = self.active_font.kerning(layer)
+			if not len(current_kerning) and import_empty == QtGui.QMessageBox.No: continue
+			
 			temp_set = set()
-
 			for pair in current_kerning.keys():
 				left, right = pair.asTuple()
 				temp_set.add((left.asTuple()[0], right.asTuple()[0]))
