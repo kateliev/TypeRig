c53e22ddf4384fb220ea7079b804919d1a73548d
Vassil Kateliev <vassil@kateliev.com>
2021-03-07 00:43:32 +0200

    Py3 Fixes: Move GUI module

---
+++ Lib/typerig/proxy/fl/gui/__init__.py
@@ -0,0 +1,10 @@
+# MODULE: TypeRig / Proxy / Fontlab / GUI
+# -----------------------------------------------------------
+# (C) Vassil Kateliev, 2017-2021 	(http://www.kateliev.com)
+# (C) Karandash Type Foundry 		(http://www.karandash.eu)
+#------------------------------------------------------------
+# www.typerig.com
+
+# No warranties. By using this you agree
+# that you use it at your own risk!
+
