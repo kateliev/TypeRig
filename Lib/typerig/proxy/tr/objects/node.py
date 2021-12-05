799a56ddf4bf582b852f50c86b8dc846b7a6e97b
Vassil Kateliev <vassil@kateliev.com>
2021-12-06 00:07:37 +0200

    [Proxy] [FL] Node [Update] 0.2.1 - simplified dynamic linking to host;

--- Lib/typerig/proxy/tr/objects/node.py
+++ Lib/typerig/proxy/tr/objects/node.py
@@ -18,7 +18,7 @@ from typerig.core.objects.node import Node
 from typerig.core.func.utils import isMultiInstance
 
 # - Init ---------------------------------
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 	
 # - Classes -------------------------------
 class trNode(Node):
@@ -36,13 +36,8 @@ class trNode(Node):
 	__slots__ = ('host', 'x', 'y', 'type', 'name', 'smooth', 'g2', 'selected', 'angle', 'transform', 'identifier','complex_math', 'parent', 'lib')
 	__meta__ = {'x':'x', 'y':'y', 'type':'type', 'g2':'g2', 'smooth':'smooth', 'name':'name', 'selected':'selected'}
 
-	# - Connect to host dynamically	
-	for src, dst in __meta__.items():
-		exec("{0} = property(lambda self: self.host.__getattribute__('{1}'), lambda self, value: self.host.__setattr__('{1}', value))".format(src, dst))
-		
-	# - Initialize 
+	# - Initialize ---------------------------
 	def __init__(self, *args, **kwargs):
-		init_dict = kwargs if kwargs is not None else {}
 
 		if len(args) == 1:
 			if isinstance(args[0], self.__class__): # Clone
@@ -56,13 +51,28 @@ class trNode(Node):
 			if isinstance(args[0], (tuple, list)):
 				x, y = args[0]
 				node_type = kwargs.get('type', 'on')
+				self.host = fl6.flNode(x, y, nodeType=node_type)
 				
 		elif len(args) == 2:
 			if isMultiInstance(args, (float, int)):
 				x, y = float(args[0]), float(args[1])
 				node_type = kwargs.get('type', 'on')
+				self.host = fl6.flNode(x, y, nodeType=node_type)
 
 		super(trNode, self).__init__(x, y, proxy=True, **kwargs)
+
+	# - Internals ------------------------------
+	def __getattribute__(self, name):
+		if name in trNode.__meta__.keys():
+			return self.host.__getattribute__(trNode.__meta__[name])
+		else:
+			return Node.__getattribute__(self, name)
+
+	def __setattr__(self, name, value):
+		if name in trNode.__meta__.keys():
+			self.host.__setattr__(trNode.__meta__[name], value)
+		else:
+			Node.__setattr__(self, name, value)
 
 	# - Basics ---------------------------------
 	def clone(self):
