319dcfdbaa0b356ee3af0a9d60916c2678b3b9af
Kateliev <vassil@kateliev.com>
2022-03-07 15:20:03 +0200

    TypeRig Tools: Match Contours [Tool] 2.2 - Set start points WIP;

--- Scripts/TypeRig Tools/TR-MatchContours.py
+++ Scripts/TypeRig Tools/TR-MatchContours.py
@@ -15,7 +15,8 @@ import warnings
 import fontlab as fl6
 import fontgate as fgt
 
-from typerig.proxy.fl.objects.glyph import eGlyph, pGlyph
+from typerig.proxy.fl.objects.glyph import eGlyph
+from typerig.proxy.fl.objects.shape import eShape
 from typerig.core.base.message import *
 
 from PythonQt import QtCore
@@ -27,7 +28,7 @@ global pLayers
 global pMode
 pLayers = None
 pMode = 0
-app_name, app_version = 'TR | Match Contours', '2.1'
+app_name, app_version = 'TR | Match Contours', '2.2'
 
 # - Configuration ----------------------
 color_foreground = QtGui.QColor('gray')
@@ -40,11 +41,13 @@ colors_tuples = [(255, 0, 0, 100),
 colors_tuples += list(permutations(range(0,256, 32), 3))
 accent_colors = [QtGui.QColor(*item) for item in colors_tuples]
 
-color_wind_ccw = QtGui.QColor(0, 255, 0, 100)
+color_wind_ccw = QtGui.QColor(0, 0, 255, 100)
 color_wind_cw = QtGui.QColor(255, 0, 0, 100)
+color_start_accent = QtGui.QColor(0, 255, 0, 255)
 
 draw_padding = 100
 draw_size = 100
+draw_radius = 16
 
 # - Sub widgets ------------------------
 class TRWContourView(QtGui.QTableWidget):
@@ -113,9 +116,33 @@ class TRWContoursOrder(QtGui.QWidget):
 		# -- Table
 		self.tab_glyphs = TRWContourView(glyph_pixmaps, self)
 
+		# -- Buttons
+		self.btn_sort_x = QtGui.QPushButton('Sort X Only')
+		self.btn_sort_y = QtGui.QPushButton('Sort Y Only')
+		
+		self.btn_sort_LRBT = QtGui.QPushButton('Left -> Right and Bottom -> Top')
+		self.btn_sort_LRTB = QtGui.QPushButton('Left -> Right and Top -> Bottom')
+		
+		self.btn_sort_x.setToolTip('Use ALT+CLICK to reverse order')
+		self.btn_sort_y.setToolTip('Use ALT+CLICK to reverse order')
+		self.btn_sort_LRBT.setToolTip('Use ALT+CLICK to reverse order')
+		self.btn_sort_LRTB.setToolTip('Use ALT+CLICK to reverse order')
+		
+		self.btn_sort_x.clicked.connect(lambda: self.set_order((True, None)))
+		self.btn_sort_y.clicked.connect(lambda: self.set_order((None, True)))
+		self.btn_sort_LRBT.clicked.connect(lambda: self.set_order((True, True)))
+		self.btn_sort_LRTB.clicked.connect(lambda: self.set_order((True, False)))
+
 		# - Layout
+		lay_tail = QtGui.QHBoxLayout()
+		lay_tail.addWidget(self.btn_sort_x)
+		lay_tail.addWidget(self.btn_sort_y)
+		lay_tail.addWidget(self.btn_sort_LRBT)
+		lay_tail.addWidget(self.btn_sort_LRTB)
+
 		lay_main = QtGui.QVBoxLayout()
 		lay_main.addWidget(self.tab_glyphs)
+		lay_main.addLayout(lay_tail)
 		
 		# - Set 
 		self.setLayout(lay_main)
@@ -206,6 +233,27 @@ class TRWContoursOrder(QtGui.QWidget):
 		work_shape.update()
 
 		self.refresh()
+
+	def set_order(self, sort_order):
+		work_layers = self.aux.glyph.masters()
+		modifiers = QtGui.QApplication.keyboardModifiers()
+		
+		reverse_order = []
+		for item in sort_order:
+			new_item = not item if item is not None else None
+			reverse_order.append(new_item)
+
+		for layer in work_layers:
+			work_shapes = self.aux.glyph.shapes(layer.name, extend=eShape)
+
+			for shape in work_shapes:
+				if modifiers == QtCore.Qt.AltModifier:
+					shape.contourOrder(reverse_order)
+				else:
+					shape.contourOrder(sort_order)
+
+		self.refresh()
+		
 
 class TRWContoursWinding(QtGui.QWidget):
 	'''Match layers by changing the contours winding direction via drag and drop'''
@@ -328,6 +376,143 @@ class TRWContoursWinding(QtGui.QWidget):
 			work_layer_contours[contour_index].reverse()
 
 		self.refresh()
+
+class TRWContoursStart(QtGui.QWidget):
+	'''Match layers by changing the contours start point'''
+	def __init__(self, aux):
+		super(TRWContoursStart, self).__init__()
+
+		# - Init
+		self.aux = aux
+		glyph_pixmaps = self.__pixmaps_refresh()
+
+		# -- Table
+		self.tab_glyphs = TRWContourView(glyph_pixmaps, self)
+
+		# -- Buttons
+		self.btn_BL = QtGui.QPushButton('Bottom Left')
+		self.btn_TL = QtGui.QPushButton('Top Left')
+		self.btn_BR = QtGui.QPushButton('Bottom Right')
+		self.btn_TR = QtGui.QPushButton('Top Right')
+
+		# - Layout
+		lay_tail = QtGui.QHBoxLayout()
+		lay_tail.addWidget(self.btn_BL)
+		lay_tail.addWidget(self.btn_TL)
+		lay_tail.addWidget(self.btn_BR)
+		lay_tail.addWidget(self.btn_TR)
+
+		lay_main = QtGui.QVBoxLayout()
+		lay_main.addWidget(self.tab_glyphs)
+		lay_main.addLayout(lay_tail)
+		
+		# - Set 
+		self.setLayout(lay_main)
+		self.setMinimumSize(300, self.sizeHint.height())
+
+	# - Internal functions ------------------------
+	def __pixmaps_refresh(self):
+		# - Init
+		return_pixmaps = []
+
+		for layer in self.aux.glyph.masters():
+			return_pixmaps.append((layer.name, self.__drawIcons(layer.getContours())))
+		
+		return return_pixmaps
+
+	def __drawIcons(self, contours):
+		# - Init
+		return_icons = []
+		sep_contours = []
+
+		# -- Prepare contours
+		cloned_contours = [contour.clone() for contour in contours]
+		main_shape = fl6.flShape()
+
+		for cid in range(len(cloned_contours)):
+			main_shape.addContour(cloned_contours[cid], True)
+
+		for cid in range(len(cloned_contours)):
+			contour_shape = fl6.flShape()
+			contour_shape.addContour(cloned_contours[cid], True)
+			sep_contours.append((contour_shape, main_shape.clone()))
+
+		# - Prepare
+		main_painter = QtGui.QPainter()
+		shape_bbox = main_shape.boundingBox
+		draw_dimension = max(shape_bbox.width(), shape_bbox.height()) + 2*draw_padding
+
+		for cid, draw_contours in enumerate(sep_contours):
+			new_icon = QtGui.QIcon()
+			cont_painter= QtGui.QPainter()
+			cont_pixmap = QtGui.QPixmap(draw_dimension, draw_dimension)
+			
+			cont_shape, other_shape = draw_contours
+
+			# - Draw Background
+			cont_painter.begin(cont_pixmap)
+			cont_painter.setBrush(QtGui.QBrush(color_background))
+			cont_painter.drawRect(QtCore.QRectF(0, 0, draw_dimension, draw_dimension))
+
+			# - Draw all other contours
+			cont_painter.setBrush(QtGui.QBrush(color_foreground))
+			cont_painter.setPen(QtGui.QPen(color_basepen, 10, QtCore.Qt.SolidLine))
+			new_transform = other_shape.transform.translate(-shape_bbox.x() + draw_padding, -shape_bbox.y() + draw_padding)
+			other_shape.applyTransform(new_transform)
+			other_shape.ensurePaths()
+			cont_painter.drawPath(other_shape.closedPath)
+
+			# - Draw the specific contour
+			# -- Set different colors accents based on winding direction
+			color_accent = color_wind_ccw if cont_shape.contours[0].clockwise else color_wind_cw
+			
+			srong_accent = QtGui.QColor(color_accent)
+			weak_accent = QtGui.QColor(color_accent)
+			
+			weak_accent.setAlpha(50)
+			srong_accent.setAlpha(255)
+
+			cont_painter.setBrush(QtGui.QBrush(weak_accent))
+			cont_painter.setPen(QtGui.QPen(srong_accent, 10, QtCore.Qt.SolidLine))
+			new_transform = cont_shape.transform.translate(-shape_bbox.x() + draw_padding, -shape_bbox.y() + draw_padding)
+			cont_shape.applyTransform(new_transform)
+			cont_shape.ensurePaths()
+
+			# - Draw path
+			draw_path = cont_shape.closedPath
+			cont_painter.drawPath(draw_path)
+
+			# - Draw nodes
+			cont_painter.setBrush(QtGui.QBrush(srong_accent))
+
+			for i in range(draw_path.elementCount()):
+				if i == 0:
+					cont_painter.setBrush(QtGui.QBrush(color_start_accent))
+					cont_painter.setPen(QtGui.QPen(color_start_accent, 10, QtCore.Qt.SolidLine))
+				else:
+					cont_painter.setBrush(QtGui.QBrush(srong_accent))
+					cont_painter.setPen(QtGui.QPen(srong_accent, 10, QtCore.Qt.SolidLine))
+
+				node_info = cont_shape.getNodeAt(i, 7)
+
+				if node_info is not None and node_info.isOn():
+					node = draw_path.elementAt(i)
+					cont_painter.drawEllipse(node.x - draw_radius, node.y - draw_radius, draw_radius*2, draw_radius*2)
+
+			new_icon.addPixmap(cont_pixmap.transformed(QtGui.QTransform().scale(1, -1)))
+
+			return_icons.append(new_icon)
+
+		return return_icons
+
+	# - Procedures ---------------------------------------
+	def refresh(self):
+		glyph_pixmaps = self.__pixmaps_refresh()
+		self.tab_glyphs.set_table(glyph_pixmaps)
+
+	def drop_item(self, lid, oid, nid):
+		return
+
 	
 # - Dialogs ------------------------------------------------
 class typerig_match(QtGui.QDialog):
@@ -335,16 +520,18 @@ class typerig_match(QtGui.QDialog):
 		super(typerig_match, self).__init__()
 	
 		# - Init --------------------------
-		self.glyph = pGlyph()
+		self.glyph = eGlyph()
 
 		# - Tabs --------------------------
 		self.tab_tools = TRHTabWidget()
 		
 		self.tool_contour_order = TRWContoursOrder(self)
 		self.tool_contour_wind = TRWContoursWinding(self)
+		self.tool_contour_start = TRWContoursStart(self)
 		
 		self.tab_tools.addTab(self.tool_contour_order, 'Contour Order')
 		self.tab_tools.addTab(self.tool_contour_wind, 'Contour Winding')
+		self.tab_tools.addTab(self.tool_contour_start, 'Contour Start')
 
 		# -- Buttons
 		self.btn_refresh = QtGui.QPushButton('&Refresh')
@@ -359,9 +546,8 @@ class typerig_match(QtGui.QDialog):
 		lay_tail.addWidget(self.btn_apply)
 
 		lay_main = QtGui.QVBoxLayout() 
-		lay_main.setContentsMargins(0,0,0,0)
-		lay_main.addWidget(self.tab_tools)
 		lay_main.addLayout(lay_tail)
+		lay_main.addWidget(self.tab_tools)
 
 		# - Set Widget -------------------------------
 		self.setLayout(lay_main)
@@ -372,11 +558,12 @@ class typerig_match(QtGui.QDialog):
 
 	def tools_refresh(self):
 		# - Init
-		self.glyph = pGlyph()
+		self.glyph = eGlyph()
 
 		# - Match contour order
 		self.tool_contour_order.refresh()
 		self.tool_contour_wind.refresh()
+		self.tool_contour_start.refresh()
 
 	def tools_update(self):
 		self.glyph.updateObject(self.glyph.fl, '{} | Glyph: {}'.format(app_name, self.glyph.name))
