cb247c8046f5205aa2770605d7c0da8c9fd8c95f
Vassil Kateliev <vassil@kateliev.com>
2022-07-21 00:44:17 +0300

    TypeRig [Toolbars]: Work in progress... - Node Toolbar: Alignment [Update] 1.16 [Beta]; - Node Toolbar: Slope [New] 1.15 [Beta];

--- Scripts/TypeRig GUI/Toolbar/node_align.py
+++ Scripts/TypeRig GUI/Toolbar/node_align.py
@@ -24,7 +24,7 @@ from typerig.proxy.fl.gui.widgets import getTRIconFont, getProcessGlyphs
 
 
 # - Init --------------------------
-tool_version = '1.15'
+tool_version = '1.16'
 tool_name = 'TypeRig Nodes: Align'
 
 TRToolFont = getTRIconFont()
@@ -58,14 +58,14 @@ class TRExternalToolBar(QtGui.QToolBar):
 		self.chk_shift_smart = QtGui.QAction("shift_smart", self.grp_align_options_shift)
 		self.chk_shift_smart.setFont(TRToolFont)
 		self.addAction(self.chk_shift_smart)
-		self.chk_shift_smart.setToolTip("Smart Shift")
+		self.chk_shift_smart.setToolTip("Smart Shift: Shift oncurve nodes together with their respective offcurve nodes even when they are not explicitly selected,")
 		self.chk_shift_smart.setCheckable(True)
 		self.chk_shift_smart.setChecked(False)
 
 		self.chk_shift_dumb = QtGui.QAction("shift_dumb", self.grp_align_options_shift)
 		self.chk_shift_dumb.setFont(TRToolFont)
 		self.addAction(self.chk_shift_dumb)
-		self.chk_shift_dumb.setToolTip("Smart Shift")
+		self.chk_shift_dumb.setToolTip("Simple Shift: Shift only selected nodes.")
 		self.chk_shift_dumb.setCheckable(True)
 		self.chk_shift_dumb.setChecked(True)
 
@@ -120,6 +120,18 @@ class TRExternalToolBar(QtGui.QToolBar):
 		self.addAction(self.btn_node_align_selection_y)
 		self.btn_node_align_selection_y.triggered.connect(lambda: TRNodeActionCollector.nodes_align(pMode, pLayers, 'E', self.chk_shift_intercept.isChecked(), self.chk_shift_keep_dimension.isChecked(), self.chk_shift_smart.isChecked()))
 
+		self.btn_node_align_outline_x = QtGui.QAction("node_align_outline_x", self.grp_align_actions)
+		self.btn_node_align_outline_x.setToolTip("Align selected nodes to the horizontal middle of outline bounding box.")
+		self.btn_node_align_outline_x.setFont(TRToolFont)
+		self.addAction(self.btn_node_align_outline_x)
+		self.btn_node_align_outline_x.triggered.connect(lambda: TRNodeActionCollector.nodes_align(pMode, pLayers, 'BBoxCenterX', self.chk_shift_intercept.isChecked(), self.chk_shift_keep_dimension.isChecked(), self.chk_shift_smart.isChecked()))
+
+		self.btn_node_align_outline_y = QtGui.QAction("node_align_outline_y", self.grp_align_actions)
+		self.btn_node_align_outline_y.setToolTip("Align selected nodes to the vertical middle of outline bounding box.")
+		self.btn_node_align_outline_y.setFont(TRToolFont)
+		self.addAction(self.btn_node_align_outline_y)
+		self.btn_node_align_outline_y.triggered.connect(lambda: TRNodeActionCollector.nodes_align(pMode, pLayers, 'BBoxCenterY', self.chk_shift_intercept.isChecked(), self.chk_shift_keep_dimension.isChecked(), self.chk_shift_smart.isChecked()))
+		
 		self.btn_node_align_neigh_x = QtGui.QAction("node_align_neigh_x", self.grp_align_actions)
 		self.btn_node_align_neigh_x.setToolTip("Align selected node in the horizontal middle of its direct neighbors")
 		self.btn_node_align_neigh_x.setFont(TRToolFont)
@@ -132,18 +144,55 @@ class TRExternalToolBar(QtGui.QToolBar):
 		self.addAction(self.btn_node_align_neigh_y)
 		self.btn_node_align_neigh_y.triggered.connect(lambda: TRNodeActionCollector.nodes_align(pMode, pLayers, 'peerCenterY', self.chk_shift_intercept.isChecked(), self.chk_shift_keep_dimension.isChecked(), self.chk_shift_smart.isChecked()))
 
-		self.btn_node_align_outline_x = QtGui.QAction("node_align_outline_x", self.grp_align_actions)
-		self.btn_node_align_outline_x.setToolTip("Align selected nodes to the horizontal middle of outline bounding box.")
-		self.btn_node_align_outline_x.setFont(TRToolFont)
-		self.addAction(self.btn_node_align_outline_x)
-		self.btn_node_align_outline_x.triggered.connect(lambda: TRNodeActionCollector.nodes_align(pMode, pLayers, 'BBoxCenterX', self.chk_shift_intercept.isChecked(), self.chk_shift_keep_dimension.isChecked(), self.chk_shift_smart.isChecked()))
+		self.btn_node_align_min_max_Y = QtGui.QAction("node_align_min_max_Y", self.grp_align_actions)
+		self.btn_node_align_min_max_Y.setToolTip("Align selected nodes to an imaginary line runnig between highest and lowest node in selection")
+		self.btn_node_align_min_max_Y.setFont(TRToolFont)
+		self.addAction(self.btn_node_align_min_max_Y)
+		self.btn_node_align_min_max_Y.triggered.connect(lambda: TRNodeActionCollector.nodes_align(pMode, pLayers, 'Y', self.chk_shift_intercept.isChecked(), self.chk_shift_keep_dimension.isChecked(), self.chk_shift_smart.isChecked()))
 
-		self.btn_node_align_outline_y = QtGui.QAction("node_align_outline_y", self.grp_align_actions)
-		self.btn_node_align_outline_y.setToolTip("Align selected nodes to the vertical middle of outline bounding box.")
-		self.btn_node_align_outline_y.setFont(TRToolFont)
-		self.addAction(self.btn_node_align_outline_y)
-		self.btn_node_align_outline_y.triggered.connect(lambda: TRNodeActionCollector.nodes_align(pMode, pLayers, 'BBoxCenterY', self.chk_shift_intercept.isChecked(), self.chk_shift_keep_dimension.isChecked(), self.chk_shift_smart.isChecked()))
+		self.btn_node_align_min_max_X = QtGui.QAction("node_align_min_max_X", self.grp_align_actions)
+		self.btn_node_align_min_max_X.setToolTip("Align selected nodes to an imaginary line runnig between lowest and highest node in selection")
+		self.btn_node_align_min_max_X.setFont(TRToolFont)
+		self.addAction(self.btn_node_align_min_max_X)
+		self.btn_node_align_min_max_X.triggered.connect(lambda: TRNodeActionCollector.nodes_align(pMode, pLayers, 'X', self.chk_shift_intercept.isChecked(), self.chk_shift_keep_dimension.isChecked(), self.chk_shift_smart.isChecked()))
 
+		self.btn_node_dimension_ascender = QtGui.QAction("dimension_ascender", self.grp_align_actions)
+		self.btn_node_dimension_ascender.setToolTip("Align selected nodes to Font metrics: Ascender height")
+		self.btn_node_dimension_ascender.setFont(TRToolFont)
+		self.addAction(self.btn_node_dimension_ascender)
+		self.btn_node_dimension_ascender.triggered.connect(lambda: TRNodeActionCollector.nodes_align(pMode, pLayers, 'FontMetrics_0', self.chk_shift_intercept.isChecked(), self.chk_shift_keep_dimension.isChecked(), self.chk_shift_smart.isChecked()))
+
+		self.btn_node_dimension_caps = QtGui.QAction("dimension_caps", self.grp_align_actions)
+		self.btn_node_dimension_caps.setToolTip("Align selected nodes to Font metrics: Caps height")
+		self.btn_node_dimension_caps.setFont(TRToolFont)
+		self.addAction(self.btn_node_dimension_caps)
+		self.btn_node_dimension_caps.triggered.connect(lambda: TRNodeActionCollector.nodes_align(pMode, pLayers, 'FontMetrics_1', self.chk_shift_intercept.isChecked(), self.chk_shift_keep_dimension.isChecked(), self.chk_shift_smart.isChecked()))
+
+		self.btn_node_dimension_xheight = QtGui.QAction("dimension_xheight", self.grp_align_actions)
+		self.btn_node_dimension_xheight.setToolTip("Align selected nodes to Font metrics: X height")
+		self.btn_node_dimension_xheight.setFont(TRToolFont)
+		self.addAction(self.btn_node_dimension_xheight)
+		self.btn_node_dimension_xheight.triggered.connect(lambda: TRNodeActionCollector.nodes_align(pMode, pLayers, 'FontMetrics_3', self.chk_shift_intercept.isChecked(), self.chk_shift_keep_dimension.isChecked(), self.chk_shift_smart.isChecked()))
+
+		self.btn_node_dimension_baseline = QtGui.QAction("dimension_baseline", self.grp_align_actions)
+		self.btn_node_dimension_baseline.setToolTip("Align selected nodes to Font metrics: Baseline")
+		self.btn_node_dimension_baseline.setFont(TRToolFont)
+		self.addAction(self.btn_node_dimension_baseline)
+		self.btn_node_dimension_baseline.triggered.connect(lambda: TRNodeActionCollector.nodes_align(pMode, pLayers, 'FontMetrics_4', self.chk_shift_intercept.isChecked(), self.chk_shift_keep_dimension.isChecked(), self.chk_shift_smart.isChecked()))
+
+		self.btn_node_dimension_descender = QtGui.QAction("dimension_descender", self.grp_align_actions)
+		self.btn_node_dimension_descender.setToolTip("Align selected nodes to Font metrics: X height")
+		self.btn_node_dimension_descender.setFont(TRToolFont)
+		self.addAction(self.btn_node_dimension_descender)
+		self.btn_node_dimension_descender.triggered.connect(lambda: TRNodeActionCollector.nodes_align(pMode, pLayers, 'FontMetrics_2', self.chk_shift_intercept.isChecked(), self.chk_shift_keep_dimension.isChecked(), self.chk_shift_smart.isChecked()))
+
+		self.btn_node_dimension_guide = QtGui.QAction("dimension_guide", self.grp_align_actions)
+		self.btn_node_dimension_guide.setToolTip("Align selected nodes to Measurment line")
+		self.btn_node_dimension_guide.setFont(TRToolFont)
+		self.addAction(self.btn_node_dimension_guide)
+		self.btn_node_dimension_guide.triggered.connect(lambda: TRNodeActionCollector.nodes_align(pMode, pLayers, 'FontMetrics_5', self.chk_shift_intercept.isChecked(), self.chk_shift_keep_dimension.isChecked(), self.chk_shift_smart.isChecked()))
+
+		# !!! To Implement: Align to BBox + % + u with dialog
 
 # - RUN ------------------------------
 if __name__ == '__main__':
---
+++ Scripts/TypeRig GUI/Toolbar/node_slope.py
@@ -0,0 +1,116 @@
+#FLM: TypeRig: Node Toolbar
+# -----------------------------------------------------------
+# (C) Vassil Kateliev, 2018-2022  	(http://www.kateliev.com)
+#------------------------------------------------------------
+# www.typerig.com
+
+# No warranties. By using this you agree
+# that you use it at your own risk!
+
+# - Dependencies -----------------
+from __future__ import absolute_import, print_function
+from collections import OrderedDict
+
+import fontlab as fl6
+from PythonQt import QtCore
+
+from typerig.proxy.fl.objects.font import pFont
+from typerig.proxy.fl.objects.glyph import eGlyph
+
+from typerig.proxy.fl.actions.node import TRNodeActionCollector
+from typerig.proxy.fl.application.app import pWorkspace
+from typerig.proxy.fl.gui import QtGui
+from typerig.proxy.fl.gui.widgets import getTRIconFont, getProcessGlyphs
+
+
+# - Init --------------------------
+tool_version = '1.00'
+tool_name = 'TypeRig Nodes: Slope'
+
+TRToolFont = getTRIconFont()
+app = pWorkspace()
+
+# -- Global parameters
+global pMode
+global pLayers
+pMode = 0
+pLayers = (True, False, False, False)
+
+# -- Helpers ------------------------------
+def get_modifier(keyboard_modifier=QtCore.Qt.AltModifier):
+	modifiers = QtGui.QApplication.keyboardModifiers()
+	return modifiers == keyboard_modifier
+
+# -- Main Widget --------------------------
+class TRExternalToolBar(QtGui.QToolBar):
+	def __init__(self, *args, **kwargs):
+		super(TRExternalToolBar, self).__init__(*args, **kwargs)
+
+		# - Init 
+		self.setWindowTitle("{} : {}".format(tool_name, tool_version))
+		self.slope_bank = {}
+
+		# - Groups
+		self.grp_slope_options = QtGui.QActionGroup(self)
+		self.grp_slope_actions = QtGui.QActionGroup(self)
+
+		# - Options
+		self.chk_slope_copy = QtGui.QAction("slope_copy", self.grp_slope_options)
+		self.chk_slope_copy.setFont(TRToolFont)
+		self.addAction(self.chk_slope_copy)
+		self.chk_slope_copy.setToolTip("Copy slope between selected nodes")
+		self.chk_slope_copy.setCheckable(True)
+		self.chk_slope_copy.setChecked(False)
+		self.chk_slope_copy.triggered.connect(self.act_slope_copy)
+
+		self.chk_slope_italic = QtGui.QAction("slope_italic", self.grp_slope_options)
+		self.chk_slope_italic.setFont(TRToolFont)
+		self.addAction(self.chk_slope_italic)
+		self.chk_slope_italic.setToolTip("Use fonts italic angle as slope")
+		self.chk_slope_italic.setCheckable(True)
+		self.chk_slope_italic.setChecked(False)
+		self.chk_slope_italic.triggered.connect(self.act_slope_italic)
+
+		# - Actions
+		self.btn_slope_paste_min = QtGui.QAction("slope_paste_min", self.grp_slope_actions)
+		self.btn_slope_paste_min.setToolTip("Paste slope to selected nodes pivoting around the one with lowest vertical coordinates")
+		self.btn_slope_paste_min.setFont(TRToolFont)
+		self.addAction(self.btn_slope_paste_min)
+		self.btn_slope_paste_min.triggered.connect(lambda: TRNodeActionCollector.slope_paste(eGlyph(), pLayers, self.slope_bank, (False, False)))
+
+		self.btn_slope_paste_max = QtGui.QAction("slope_paste_max", self.grp_slope_actions)
+		self.btn_slope_paste_max.setToolTip("Paste slope to selected nodes pivoting around the one with highest vertical coordinates")
+		self.btn_slope_paste_max.setFont(TRToolFont)
+		self.addAction(self.btn_slope_paste_max)
+		self.btn_slope_paste_max.triggered.connect(lambda: TRNodeActionCollector.slope_paste(eGlyph(), pLayers, self.slope_bank, (True, False)))
+
+		self.btn_slope_paste_min_flip = QtGui.QAction("slope_paste_min_flip", self.grp_slope_actions)
+		self.btn_slope_paste_min_flip.setToolTip("Paste horizontally flipped slope to selected nodes pivoting around the one with lowest vertical coordinates")
+		self.btn_slope_paste_min_flip.setFont(TRToolFont)
+		self.addAction(self.btn_slope_paste_min_flip)
+		self.btn_slope_paste_min_flip.triggered.connect(lambda: TRNodeActionCollector.slope_paste(eGlyph(), pLayers, self.slope_bank, (False, True)))
+
+		self.btn_slope_paste_max_flip = QtGui.QAction("slope_paste_max_flip", self.grp_slope_actions)
+		self.btn_slope_paste_max_flip.setToolTip("Paste horizontally flipped slope to selected nodes pivoting around the one with highest vertical coordinates")
+		self.btn_slope_paste_max_flip.setFont(TRToolFont)
+		self.addAction(self.btn_slope_paste_max_flip)
+		self.btn_slope_paste_max_flip.triggered.connect(lambda: TRNodeActionCollector.slope_paste(eGlyph(), pLayers, self.slope_bank, (True, True)))
+
+	# - Procedures -------------------------	
+	def act_slope_copy(self):
+		if self.chk_slope_copy.isChecked():
+			self.slope_bank = TRNodeActionCollector.slope_copy(eGlyph(), pLayers)
+		else:
+			self.slope_bank = {}
+
+	def act_slope_italic(self):
+		if self.chk_slope_italic.isChecked():
+			self.slope_bank = TRNodeActionCollector.slope_italic(eGlyph(), pLayers)
+		else:
+			self.slope_bank = {}
+
+# - RUN ------------------------------
+if __name__ == '__main__':
+	toolbar_control = TRExternalToolBar(app.main)
+	app.main.addToolBar(toolbar_control)
+
