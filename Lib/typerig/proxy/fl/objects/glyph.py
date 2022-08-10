a7e804b5309d8526b0a9fc16a0f7d4bd1944dfe7
Kateliev <vassil@kateliev.com>
2022-08-10 11:19:37 +0300

    Issue #63: TR [Proxy] Glyph [Fix] 0.30.6: Mask layer

--- Lib/typerig/proxy/fl/objects/glyph.py
+++ Lib/typerig/proxy/fl/objects/glyph.py
@@ -30,7 +30,7 @@ from typerig.proxy.fl.application.app import pWorkspace
 from typerig.proxy.fl.objects.string import diactiricalMarks
 
 # - Init -------------------------------------------
-__version__ = '0.30.5'
+__version__ = '0.30.6'
 
 # - Keep compatibility for basestring checks
 try:
@@ -267,7 +267,8 @@ class pGlyph(object):
 			return self.fl.activeLayer.getMaskLayer(force_create)
 		else:
 			if isinstance(layer, int):
-				return self.fl.layers[layer].getMaskLayer(force_create)
+				return_layer = self.fl.layers[layer]
+				return return_layer if return_layer.isMaskLayer() else return_layer.getMaskLayer(force_create)
 
 			elif isinstance(layer, basestring):
 				return self.fl.getLayerByName(layer).getMaskLayer(force_create)
